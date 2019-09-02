f3b9a2cacc830aca8e49e6d3596e9dee7418aa33
stevewhit <swhit114@gmail.com>
2019-09-02 09:40:08 -0400

    Added generic quote and company services;

---
+++ StockMarket.Generic/Services/CompanyService.cs
@@ -0,0 +1,173 @@
+﻿using Framework.Generic.EntityFramework;
+using System;
+using System.Data.Entity;
+using StockMarket.DataModel;
+using System.Linq;
+
+namespace StockMarket.Generic.Services
+{
+    public interface ICompanyService<T> : IDisposable where T : Company
+    {
+        /// <summary>
+        /// Returns stored companies.
+        /// </summary>
+        /// <returns>Returns companies stored in the repository.</returns>
+        IDbSet<T> GetCompanies();
+
+        /// <summary>
+        /// Finds and returns the company with the matching id.
+        /// </summary>
+        /// <param name="id">The id of the company to return.</param>
+        /// <returns>Returns the company with the matching id.</returns>
+        T FindCompany(int id);
+
+        /// <summary>
+        /// Adds the supplied <paramref name="company"/>.
+        /// </summary>
+        /// <param name="company">The company that is to be added.</param>
+        void Add(T company);
+
+        /// <summary>
+        /// Updates the supplied <paramref name="company"/>.
+        /// </summary>
+        /// <param name="company">The company that is to be updated.</param>
+        void Update(T company);
+
+        /// <summary>
+        /// Finds and deletes an existing company by <paramref name="id"/>.
+        /// </summary>
+        /// <param name="id">The id of company to be deleted.</param>
+        void Delete(int id);
+
+        /// <summary>
+        /// Deletes the supplied <paramref name="company"/>.
+        /// </summary>
+        /// <param name="company">The company that is to be deleted.</param>
+        void Delete(T company);
+    }
+
+    public class CompanyService<T> : ICompanyService<T> where T : Company
+    {
+        private readonly IEfRepository<T> _repository;
+        private bool _isDisposed = false;
+
+        public CompanyService(IEfRepository<T> repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException("repository");
+        }
+
+        #region ICompanyService<C>
+
+        /// <summary>
+        /// Returns stored companies.
+        /// </summary>
+        /// <returns>Returns companies stored in the repository.</returns>
+        public IDbSet<T> GetCompanies()
+        {
+            if (_isDisposed)
+                throw new ObjectDisposedException("CompanyService", "The service has been disposed.");
+
+            return _repository.GetEntities();
+        }
+
+        /// <summary>
+        /// Finds and returns the company with the matching id.
+        /// </summary>
+        /// <param name="id">The id of the company to return.</param>
+        /// <returns>Returns the company with the matching id.</returns>
+        public T FindCompany(int id)
+        {
+            return GetCompanies().FirstOrDefault(c => c.Id == id);
+        }
+
+        /// <summary>
+        /// Adds the supplied <paramref name="company"/>.
+        /// </summary>
+        /// <param name="company">The company that is to be added.</param>
+        public void Add(T company)
+        {
+            if (_isDisposed)
+                throw new ObjectDisposedException("CompanyService", "The service has been disposed.");
+
+            if (company == null)
+                throw new ArgumentNullException("company");
+
+            _repository.Add(company);
+            _repository.SaveChanges();
+        }
+
+        /// <summary>
+        /// Updates the supplied <paramref name="company"/>.
+        /// </summary>
+        /// <param name="company">The company that is to be updated.</param>
+        public void Update(T company)
+        {
+            if (_isDisposed)
+                throw new ObjectDisposedException("CompanyService", "The service has been disposed.");
+
+            if (company == null)
+                throw new ArgumentNullException("company");
+
+            _repository.Update(company);
+            _repository.SaveChanges();
+        }
+
+        /// <summary>
+        /// Finds and deletes an existing company by <paramref name="id"/>.
+        /// </summary>
+        /// <param name="id">The id of company to be deleted.</param>
+        public void Delete(int id)
+        {
+            var company = FindCompany(id);
+
+            if (company == null)
+                throw new ArgumentException($"A company with the supplied id doesn't exist: {id}.");
+
+            Delete(company);
+        }
+
+        /// <summary>
+        /// Deletes the supplied <paramref name="company"/>.
+        /// </summary>
+        /// <param name="company">The company that is to be deleted.</param>
+        public void Delete(T company)
+        {
+            if (_isDisposed)
+                throw new ObjectDisposedException("CompanyService", "The service has been disposed.");
+
+            if (company == null)
+                throw new ArgumentNullException("company");
+
+            _repository.Delete(company);
+            _repository.SaveChanges();
+        }
+
+        #endregion
+        #region IDisposable
+        /// <summary>
+        /// Disposes this object and properly cleans up resources. 
+        /// </summary>
+        protected void Dispose(bool disposing)
+        {
+            if (!_isDisposed)
+            {
+                if (disposing)
+                {
+                    _repository.Dispose();
+                }
+
+                _isDisposed = true;
+            }
+        }
+
+        /// <summary>
+        /// Disposes this object and properly cleans up resources. 
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
---
+++ StockMarket.Generic/Services/QuoteService.cs
@@ -0,0 +1,198 @@
+﻿using Framework.Generic.EntityFramework;
+using System;
+using System.Data.Entity;
+using StockMarket.DataModel;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace StockMarket.Generic.Services
+{
+    public interface IQuoteService<T> : IDisposable where T : Quote
+    {
+        /// <summary>
+        /// Returns quotes stored in the repository.
+        /// </summary>
+        /// <returns>Returns quotes stored in the repository.</returns>
+        IDbSet<T> GetQuotes();
+
+        /// <summary>
+        /// Finds and returns the quote from the repository with the matching id.
+        /// </summary>
+        /// <param name="id">The id of the quote to return.</param>
+        /// <returns>Returns the quote with the matching id.</returns>
+        T FindQuote(int id);
+
+        /// <summary>
+        /// Adds the supplied <paramref name="quote"/> to the repository.
+        /// </summary>
+        /// <param name="quote">The quote that is to be added.</param>
+        void Add(T quote);
+
+        /// <summary>
+        /// Adds the supplied <paramref name="quotes"/> to the repository.
+        /// </summary>
+        /// <param name="quotes">The quotes that are to be added.</param>
+        void AddRange(IEnumerable<T> quotes);
+
+        /// <summary>
+        /// Updates the supplied <paramref name="quote"/> in the repository.
+        /// </summary>
+        /// <param name="quote">The quote that is to be updated.</param>
+        void Update(T quote);
+
+        /// <summary>
+        /// Finds and deletes an existing quote in the repository by using its <paramref name="id"/>.
+        /// </summary>
+        /// <param name="id">The id of quote to be deleted.</param>
+        void Delete(int id);
+
+        /// <summary>
+        /// Deletes the supplied <paramref name="quote"/> from the repository.
+        /// </summary>
+        /// <param name="quote">The quote that is to be deleted.</param>
+        void Delete(T quote);
+    }
+
+    public class QuoteService<T> : IQuoteService<T> where T : Quote
+    {
+        private readonly IEfRepository<T> _repository;
+        private bool _isDisposed = false;
+
+        public QuoteService(IEfRepository<T> repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException("repository");
+        }
+
+        #region IQuoteService<T>
+
+        /// <summary>
+        /// Returns quotes stored in the repository.
+        /// </summary>
+        /// <returns>Returns quotes stored in the repository.</returns>
+        public IDbSet<T> GetQuotes()
+        {
+            if (_isDisposed)
+                throw new ObjectDisposedException("QuoteService", "The service has been disposed.");
+
+            return _repository.GetEntities();
+        }
+
+        /// <summary>
+        /// Finds and returns the quote from the repository with the matching id.
+        /// </summary>
+        /// <param name="id">The id of the quote to return.</param>
+        /// <returns>Returns the quote with the matching id.</returns>
+        public T FindQuote(int id)
+        {
+            return GetQuotes().FirstOrDefault(c => c.Id == id);
+        }
+        
+        /// <summary>
+        /// Adds the supplied <paramref name="quote"/> to the repository.
+        /// </summary>
+        /// <param name="quote">The quote that is to be added.</param>
+        public void Add(T quote)
+        {
+            if (_isDisposed)
+                throw new ObjectDisposedException("QuoteService", "The service has been disposed.");
+
+            if (quote == null)
+                throw new ArgumentNullException("quote");
+            
+            _repository.Add(quote);
+            _repository.SaveChanges();
+        }
+
+        /// <summary>
+        /// Adds the supplied <paramref name="quotes"/> to the repository.
+        /// </summary>
+        /// <param name="quotes">The quotes that are to be added.</param>
+        public void AddRange(IEnumerable<T> quotes)
+        {
+            if (_isDisposed)
+                throw new ObjectDisposedException("QuoteService", "The service has been disposed.");
+
+            if (quotes == null)
+                throw new ArgumentNullException("quotes");
+
+            _repository.AddRange(quotes);
+            _repository.SaveChanges();
+        }
+
+        /// <summary>
+        /// Updates the supplied <paramref name="quote"/> in the repository.
+        /// </summary>
+        /// <param name="quote">The quote that is to be updated.</param>
+        public void Update(T quote)
+        {
+            if (_isDisposed)
+                throw new ObjectDisposedException("QuoteService", "The service has been disposed.");
+
+            if (quote == null)
+                throw new ArgumentNullException("quote");
+            
+            _repository.Update(quote);
+            _repository.SaveChanges();
+        }
+
+        /// <summary>
+        /// Finds and deletes an existing quote in the repository by using its <paramref name="id"/>.
+        /// </summary>
+        /// <param name="id">The id of quote to be deleted.</param>
+        public void Delete(int id)
+        {
+            var quote = FindQuote(id);
+
+            if (quote == null)
+                throw new ArgumentException($"A quote with the supplied id doesn't exist: {id}.");
+
+            Delete(quote);
+        }
+
+        /// <summary>
+        /// Deletes the supplied <paramref name="quote"/> from the repository.
+        /// </summary>
+        /// <param name="quote">The quote that is to be deleted.</param>
+        public void Delete(T quote)
+        {
+            if (_isDisposed)
+                throw new ObjectDisposedException("QuoteService", "The service has been disposed.");
+
+            if (quote == null)
+                throw new ArgumentNullException("quote");
+
+            _repository.Delete(quote);
+            _repository.SaveChanges();
+        }
+
+        #endregion
+        #region IDisposable
+
+        /// <summary>
+        /// Disposes this object and properly cleans up resources. 
+        /// </summary>
+        protected void Dispose(bool disposing)
+        {
+            if (!_isDisposed)
+            {
+                if (disposing)
+                {
+                    _repository.Dispose();
+                }
+
+                _isDisposed = true;
+            }
+        }
+
+        /// <summary>
+        /// Disposes this object and properly cleans up resources. 
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion
+    }
+}
