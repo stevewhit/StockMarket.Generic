479a674463cd464666cabb834d320d3eb2865467
Steve Whitmire <swhit114@gmail.com>
2020-04-17 17:13:01 -0400

    Updated database scripts to create login & user for domain security group; Removed old database login and user; Updated database connection strings to use integrated security;

--- StockMarket.DataModel/Database Scripts/2 - Security/001 - Create Database User StockMarketDataAdmin.CONFIG.ALL.sql
+++
@@ -1,9 +0,0 @@
-IF NOT EXISTS (SELECT * FROM sys.sysusers WHERE [name] = 'StockMarketDataAdmin')
-BEGIN
-	PRINT 'Creating "StockMarketDataAdmin" user"..'
-
-	CREATE USER [StockMarketDataAdmin] FOR LOGIN [StockMarketDataAdmin] WITH DEFAULT_SCHEMA=[dbo]
-	GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE TO [StockMarketDataAdmin]
-END
-ELSE
-	PRINT 'The user "StockMarketDataAdmin" already exists..'
